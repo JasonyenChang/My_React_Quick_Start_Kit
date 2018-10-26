7f42f3c11f422f6f1051917dd1a9ff7d711ba4df
Jason <jason@Jasonde-MacBook-Air.local>
2018-10-26 21:42:51 +0800

    Updates

--- src/components/SideMenu/style.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-import { Menu, Icon } from 'antd';
-
-export const MenuItem = styled(Menu.Item)`
-  &.ant-menu-item-selected.ant-menu-item-selected {
-    background-color: #50B087;
-  }
-`;
