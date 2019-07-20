3c2012579b754dec1fa0d5bb8ee7e187d6be59db
Jason <jasonyen_chang@wistron.com>
2019-07-20 18:19:25 +0800

    Modify package.json

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, '../dist')));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
